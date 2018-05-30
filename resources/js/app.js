6628d60349fbad227259b9fc3837fe9f299db5ec
Raymond Nziku <reanzi@Raymonds-MacBook-Pro.local>
2018-05-30 11:05:39 +0300

    Getting input value &  passing btn modules

--- resources/js/app.js
+++ resources/js/app.js
@@ -8,26 +8,52 @@ var budgetController = (function () {
 
 // UI Ctrl
 var UIController = (function () {
-    // some code
+
+    var DOMstrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputBtn: '.add__btn',
+    };
+
+    return {
+        //expossing the private data to the public
+        getInput: function () {
+            return {
+                type: document.querySelector(DOMstrings.inputType).value,//will be either income or expenses
+                description: document.querySelector(DOMstrings.inputDescription).value,
+                value: document.querySelector(DOMstrings.inputValue).value,
+            };
+
+        },
+        getDOMstrings: function () {
+            return DOMstrings;
+        }
+    };
 })();
 
 // Global App Ctrl
 // controller  {receiving the other two module as argument}
 var controller = (function (budgetCtrl, UICtrl) {
 
+    var DOM = UICtrl.getDOMstrings();
+
     var ctrlAddItem = function () {
-        // get the field input data
+        // get the field input data from the private controller
+        var input = UICtrl.getInput();
+        console.log(input);
+
+
         // add the item to the budget controller
         // add the item to the UI
         // calc the budget
         // Display the budget on the UI
 
-        console.log('It works');
+
     }
 
     // eventListener on add button
-    var addBtn = document.querySelector('.add__btn');
-    addBtn.addEventListener('click', ctrlAddItem); // ctrlAddItem is a callBack fxn, no need of ()
+    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem); // ctrlAddItem is a callBack fxn, no need of ()
 
     // eventListener on keypress
     document.addEventListener('keypress', function (event) {
