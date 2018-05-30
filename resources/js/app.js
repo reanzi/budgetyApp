76d7982a2d5e1f6522a6574f416e475444018c7b
Raymond Nziku <reanzi@Raymonds-MacBook-Pro.local>
2018-05-30 10:24:03 +0300

    setting up event Listener and enforcing DRY principle

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,33 +1,47 @@
 // module using module pattern
 
-// budget Module
+// budget Ctrl
 var budgetController = (function () {
-    var x = 12;
-
-    var add = function (a) {
-        return x + a;
-    }
-    return {
-        publicTest: function (b) {
-            return add(b);
-        }
-    }
-})();
-
-
-// UI Module
-var UIController = (function () {
     // some code
 })();
 
 
+// UI Ctrl
+var UIController = (function () {
+    // some code
+})();
+
+// Global App Ctrl
 // controller  {receiving the other two module as argument}
 var controller = (function (budgetCtrl, UICtrl) {
-    var z = budgetCtrl.publicTest(10);
 
-    return {
-        anotherPublic: function () {
-            console.log(z);
-        }
+    var ctrlAddItem = function () {
+        // get the field input data
+        // add the item to the budget controller
+        // add the item to the UI
+        // calc the budget
+        // Display the budget on the UI
+
+        console.log('It works');
     }
+
+    // eventListener on add button
+    var addBtn = document.querySelector('.add__btn');
+    addBtn.addEventListener('click', ctrlAddItem); // ctrlAddItem is a callBack fxn, no need of ()
+
+    // eventListener on keypress
+    document.addEventListener('keypress', function (event) {
+        if (event.keyCode === 13 || event.which === 13) {
+            ctrlAddItem();
+        }
+    });
+
+
 })(budgetController, UIController);
+
+
+
+
+
+
+
