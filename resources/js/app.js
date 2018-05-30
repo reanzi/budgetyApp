8f9844c6b09d7713df59244425735743b58c5ac8
reanzi <nziku99@gmail.com>
2018-05-30 15:20:42 +0300

    added clearFields methods, it works

--- resources/js/app.js
+++ resources/js/app.js
@@ -108,6 +108,19 @@ var UIController = (function () {
             document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
         },
 
+        clearFields: function () {
+            var fields, fieldsArr;
+            fields = document.querySelectorAll(DOMstrings.inputDescription + ', ' + DOMstrings.inputValue);
+            // fields return a list, so we need to convert into array
+            fieldsArr = Array.prototype.slice.call(fields);
+
+            fieldsArr.forEach(function (current, index, array) {
+                current.value = "";
+            });
+            //after clear return focus to description input
+            fieldsArr[0].focus();
+        },
+
         getDOMstrings: function () {
             return DOMstrings;
         }
@@ -148,8 +161,12 @@ var controller = (function (budgetCtrl, UICtrl) {
         // console.log(newItem);
         // 3. add the item to the UI
         UICtrl.addListItem(newItem, input.type); //passing the newItem to the UI
-        // 4. calc the budget
-        // 5. Display the budget on the UI
+
+        // 4. Clear the input fields
+        UICtrl.clearFields();
+
+        // 5. calc the budget
+        // 6. Display the budget on the UI
 
 
     };
